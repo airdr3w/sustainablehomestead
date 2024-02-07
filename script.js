823666454aeb304656c3a12161c906b2f8650fbe
airdr3w <atkinskid1@outlook.com>
2024-02-07 13:25:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassNAme('navbar-links')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButtion.addEventListener('click', () => {
-	navbarLinks.classlist.toggle('active')
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
 })
